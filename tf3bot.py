6cba6f4caebd32ce78fd80525332ee08627bfa4b
Urauth <pee-94@hotmail.com>
2016-01-14 19:40:14 +0200

    moving paddle

--- tf3bot.py
+++ tf3bot.py
@@ -1,2 +1,23 @@
 def handle(data):
-	return data;
+
+	movement = data
+	if 'left' in data:
+		movement = movePaddle(data)
+	return movement
+
+def movePaddle(data):
+	if 'ball' in data:
+		ballY = data["ball"]["pos"]["y"]
+		
+		paddleHeight = 0
+		if 'left' in data:
+			paddleHeight = data["left"]["y"]
+
+		if(paddleHeight > ballY):
+			data = -1.0
+		else:
+			data = 1.0
+		
+		return data
+	else:
+		return 0.0
