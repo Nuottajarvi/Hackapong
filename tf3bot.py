434d25dc65add53f42b54e2e7d0051d5af126d94
feilz <akerlunddan@gmail.com>
2016-01-14 20:06:29 +0200

    asd

--- tf3bot.py
+++ tf3bot.py
@@ -1,13 +1,63 @@
 def handle(data):
+	projectedY=projectedTarget(data)
 	return data;
 
-def rotation(data):		
-	if "ball" in data:	
-		prevX=bX
-		prevY=bY
-		bX = data["ball"]["pos"]["x"]
-		bY = data["ball"]["pos"]["y"]
-		rot = (bY-prevY)/(bX-prevX)
-		return rot
+fieldHeight
+fieldWidth
 
+paddleHeight
+paddleWidth
 
+ballRadius
+tickInterval
+
+prevX
+prevY
+currX
+currY
+
+dirX
+dirY
+
+def direction(ballX, ballY):
+	global currX
+	currX=ballX
+	global currY
+	currY=ballY			
+	
+	global dirX
+	dirX=currX-prevX
+	global dirY
+	dirY=currY-prevY
+	
+	global prevY
+	prevY=currY
+	global prevX
+	prevX=currX				
+
+def collectValues(data):
+	if 'conf' in data:
+		global fieldHeight
+		fieldHeight=data["conf"]["maxHeight"]
+		global fieldWidth
+		fieldWidth=data["conf"]["maxWidth"]
+		global paddleHeight
+		paddleHeight = data["conf"]["paddleHeight"]
+		global paddleWidth
+		paddleWidth=data["conf"]["paddleWidth"]
+		global ballRadius
+		ballRadius=data["conf"]["ballRadius"]
+		global tickInterval
+		tickInterval=data["conf"]["tickInterval"]
+
+def projectedTarget(data):	
+	direction(data["ball"]["pos"]["x"],data["ball"]["pos"]["y"])	
+	calcX=currX
+	calcY=currY
+	flipped=1
+	while calcX < paddleWidth or calcX > fieldWidth - paddleWidth:
+		calcX+=currX		
+		calcY+=currY*flipped
+		if (calcY<=ballRadius or calcY>=fieldHeight-ballRadius):
+			flipped=-flipped	
+	return calcY
